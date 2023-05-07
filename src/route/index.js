195fd44e5ab1d5e78e40eeba4c2cba2a73459a76
Ruslashka <chotakoe1996@gmail.com>
2023-05-07 23:27:40 +0300

    upd packages

--- src/route/index.js
+++ src/route/index.js
@@ -599,4 +599,14 @@ router.get('/bootstrap', function (req, res) {
 })
 
 // ================================================================
+//           ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/bootstrapicon', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('bootstrapicon', {
+    layout: 'basic',
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
 module.exports = router
